d72ee4720bc39dc4210b09fd90d43bdbc72dd2ee
ADolynskyi <tolik901@gmail.com>
2023-07-04 21:03:39 +0300

    Added functionality for button, getter for text field and setter for answer label

--- src/main/java/com/spiel/cities/MainWindowController.java
+++ src/main/java/com/spiel/cities/MainWindowController.java
@@ -1,5 +1,35 @@
 package com.spiel.cities;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class MainWindowController {
+    public void setAnswerText(String answerText) {
+        this.answerText.setText(answerText);
+    }
+    @FXML
+    private Label answerText;
+
+    public String getInputText() {
+        return inputText.getText();
+    }
+
+    @FXML
+    private TextField inputText;
+
+    @FXML
+    protected void makeMove(){
+       //тут має бути виклик методу хід (Здається це в тебе turn)
+        //наступний рядок для демонстрації роботи, видалити
+        //answerText.setText(inputText.getText());
+        setAnswerText(getInputText());
+        //Cleaning text field for next input
+        inputText.setText("");
+    }
+
+
+
+
 
 }
