301770a2289d1ef804d93fb7a18d6a3c8f730cc2
ADolynskyi <tolik901@gmail.com>
2023-07-04 21:20:20 +0300

    Deleted some comments, changed variables for text field and label

--- src/main/java/com/spiel/cities/MainWindowController.java
+++ src/main/java/com/spiel/cities/MainWindowController.java
@@ -6,26 +6,23 @@ import javafx.scene.control.TextField;
 
 public class MainWindowController {
     public void setAnswerText(String answerText) {
-        this.answerText.setText(answerText);
+        this.answerLabel.setText(answerText);
     }
     @FXML
-    private Label answerText;
+    private Label answerLabel;
 
+    @FXML
+    private TextField inputField;
     public String getInputText() {
-        return inputText.getText();
+        return inputField.getText();
     }
-
-    @FXML
-    private TextField inputText;
-
     @FXML
     protected void makeMove(){
        //тут має бути виклик методу хід (Здається це в тебе turn)
         //наступний рядок для демонстрації роботи, видалити
-        //answerText.setText(inputText.getText());
         setAnswerText(getInputText());
         //Cleaning text field for next input
-        inputText.setText("");
+        inputField.setText("");
     }
 
 
